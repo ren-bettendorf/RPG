6e0ead4962dfae79fd0a3e17c689b19dd050e2a5
Ren <renaldorini@gmail.com>
2020-10-25 16:51:20 -0600

    Finished part 1

---
+++ CameraFollow.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+
+onready var target = get_node("/root/MainScene/Player")
+ 
+func _process (delta):
+	position = target.position
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
